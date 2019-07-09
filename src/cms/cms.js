b6e901517206c17e3271a9c72028598474864ec7
Josh Friend <joshwhatk@gmail.com>
2019-07-08 21:46:42 -0500

    Feature: Add creatable pages (#27)
    * Remove Product Page
    * Add Content Pages
    * Remove editorial workflow

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,9 @@ import CMS from 'netlify-cms'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ContentPagePreview from './preview-templates/ContentPagePreview'
 
 CMS.registerPreviewTemplate('homepage', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('content', ContentPagePreview)
