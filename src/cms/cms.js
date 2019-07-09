f3ffad1f2444314776a862e2ba5ecd6ef1d9516d
Josh Friend <joshwhatk@gmail.com>
2019-07-08 21:41:44 -0500

    Add Content Pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ContentPagePreview from './preview-templates/ContentPagePreview'
 
 CMS.registerPreviewTemplate('homepage', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('content', ContentPagePreview)
